dc418d7313f22489d88d247e2d20adbe19ae8a42
pulle12 <psummerauer@tsn.at>
2026-02-23 11:36:15 +0100

    feat: go on implementing index.php. creating error.php

--- index.php
+++ index.php
@@ -1,6 +1,19 @@
 <?php
 
+require_once('controller/Controller.php');
+
 // http://localhost/php/3_1_Passwortmanager/index.php?r=credentials/index
 // http://localhost/php/3_1_Passwortmanager/index.php?r=credentials/update&id=25
 
-$route = $_GET['r'];
+$route = isset($_GET['r']) ? explode('/', trim($_GET['r'])) : ['credentials'];
+$controller = sizeof($route) > 0 ? $route[0] : 'credentials';
+
+if($controller == 'credentials'){
+
+} else {
+    Controller::showError("Page not found", "Page for operation " . $controller . " was not found!", 404);
+}
+
+$action = sizeof($route) > 1 ? $route[1] : 'index';
+
+
---
+++ views/error.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="utf-8">
+    <title>Passwortmanager</title>
+
+    <link rel="shortcut icon" href="css/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="css/favicon.ico" type="image/x-icon">
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="../js/index.php"></script>
+</head>
+<body>
+
+</body>
+</html>
