ff7e0f992bc04bc934ce9fd7bb6bda5e4128af98
pulle12 <psummerauer@tsn.at>
2026-02-23 11:20:13 +0100

    feat: begin with creating new index file

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+
+// http://localhost/php/3_1_Passwortmanager/index.php?r=credentials/index
+// http://localhost/php/3_1_Passwortmanager/index.php?r=credentials/update&id=25
+
+$route = $_GET['r'];
