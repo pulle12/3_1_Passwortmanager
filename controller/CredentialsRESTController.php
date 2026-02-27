399ac6f652232660721abd3e757841968695ce76
pulle12 <psummerauer@tsn.at>
2026-02-27 21:02:17 +0100

    feat: creating new controller class

---
+++ controller/CredentialsRESTController.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once('RESTController.php');
+require_once('models/Credentials.php');
+class CredentialsRESTController extends RESTController
+{
+
+    public function handleRequest()
+    {
+        // TODO: Implement handleRequest() method.
+    }
+}
