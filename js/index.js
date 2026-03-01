762dac446bb113053e43f4f4aaa89c1f778f2c35
pulle12 <psummerauer@tsn.at>
2026-03-01 21:05:57 +0100

    feat: implementing the second js function analogue to the first one

--- js/index.js
+++ js/index.js
@@ -11,5 +11,13 @@ function loadAllCredentials() {
 }
 
 function loadFilteredCredentials() {
+    var filter = $('#filter').val();
 
+    if(filter == "") {
+        loadAllCredentials();
+    } else {
+        $.get('api/credentials/search/' + filter, function(data) {
+            console.log(data);
+        })
+    }
 }
